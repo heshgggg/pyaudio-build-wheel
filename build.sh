5aaeb3c5666f6b92207ed1e4ba2f8aa3c166c595
shanebishop <shane.bishop@outlook.com>
2022-07-04 13:27:29 -0400

    Must use -L with curl to follow redirect

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -ex
 python --version
 
 # Build and install PortAudio
-curl -sSO https://github.com/PortAudio/portaudio/archive/refs/tags/v19.7.0.zip
+curl -sSLO https://github.com/PortAudio/portaudio/archive/refs/tags/v19.7.0.zip
 unzip -q v19.7.0.zip
 (
     cd portaudio-19.7.0
